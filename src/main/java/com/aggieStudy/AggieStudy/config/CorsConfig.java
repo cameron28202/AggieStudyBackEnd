2c48293d3a26106bc29b1a52ad018f90018f0ed4
Cameron Stone <79529043+cameron28202@users.noreply.github.com>
2024-10-21 20:50:06 -0500

    cors config

--- src/main/java/com/aggieStudy/AggieStudy/config/CorsConfig.java
+++ src/main/java/com/aggieStudy/AggieStudy/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://aggie-study-front-end.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "https://www.aggiestudy.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
